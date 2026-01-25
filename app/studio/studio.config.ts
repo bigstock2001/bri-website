646992c59f85db91dbfe5863ad221108305f00ff
bigstock2001 <donald.l.dunn@gmail.com>
2026-01-25 17:49:19 -0600

    just another guess

---
+++ app/studio/studio.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "../../sanity/schemaTypes";
+
+export default defineConfig({
+  name: "default",
+  title: "bri-website-blog",
+  projectId: "m3m672rb",
+  dataset: "production",
+  plugins: [structureTool(), visionTool()],
+  schema: { types: schemaTypes },
+});
