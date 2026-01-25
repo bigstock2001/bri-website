19a47ba4e37f3f4d1d1c7341636c40f729d0a8f1
bigstock2001 <donald.l.dunn@gmail.com>
2026-01-25 17:36:00 -0600

    update

---
+++ app/studio/[[...tool]]/page.jsx
@@ -0,0 +1,9 @@
+// app/studio/[[...tool]]/page.jsx
+"use client";
+
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../sanity.config";
+
+export default function StudioPage() {
+  return <NextStudio config={config} />;
+}
--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,18 +1,19 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+// sanity.config.ts
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./sanity/schemaTypes";
 
 export default defineConfig({
-  name: 'default',
-  title: 'bri-website-blog',
+  name: "default",
+  title: "bri-website-blog",
 
-  projectId: 'm3m672rb',
-  dataset: 'production',
+  projectId: "m3m672rb",
+  dataset: "production",
 
   plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
