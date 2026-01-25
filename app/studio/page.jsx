93ef22ec62370722bf50d5383a32bd0b0532070b
bigstock2001 <donald.l.dunn@gmail.com>
2026-01-25 17:37:55 -0600

    removed conflich

--- app/studio/page.jsx
+++
@@ -1,9 +0,0 @@
-// app/studio/page.jsx
-export default function StudioPage() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Studio</h1>
-      <p>Coming soon.</p>
-    </main>
-  );
-}
