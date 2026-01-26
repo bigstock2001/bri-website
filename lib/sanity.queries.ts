ecb082b955dac1e693c390e57902da0faa2783cf
bigstock2001 <donald.l.dunn@gmail.com>
2026-01-25 20:13:52 -0600

    k

--- lib/sanity.queries.ts
+++ lib/sanity.queries.ts
@@ -11,9 +11,20 @@ export const POSTS_QUERY = groq`
   }
 `;
 
-export const POST_BY_SLUG_QUERY = groq`
-  *[_type == "post" && slug.current == $slug][0]{
+export const POSTS_WITH_BODY_QUERY = groq`
+  *[_type == "post" && defined(slug.current)]
+  | order(publishedAt desc) {
     _id,
+    title,
+    "slug": slug.current,
+    excerpt,
+    body,
+    publishedAt
+  }
+`;
+
+export const POST_BY_SLUG_QUERY = groq`
+  *[_type == "post" && slug.current == $slug][0] {
     title,
     "slug": slug.current,
     excerpt,
